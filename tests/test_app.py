54e019839c5e01d0effaf76d61abdf877737194f
Pavel Molchan <pavel.molchan@innowise.com>
2025-09-16 14:10:50 +0300

    fix: Update test assertion for new UI

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,4 @@ def client():
 def test_home_page(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Full Cycle CI/CD Project" in response.data
+    assert b"DevOps Project Status" in response.data
